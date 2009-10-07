a7e1ba479d2d022b19584e39fcd4637c0179b557
kazuho <kazuho@4d3e2a30-9d6d-0410-bc8c-dac56cff10b3>
2009-10-07 04:51:38 +0000

    insert PREREQ_PM into Makefile
    git-svn-id: http://kazuho.31tools.com/svn/incline/trunk@200 4d3e2a30-9d6d-0410-bc8c-dac56cff10b3

--- META.yml.pl
+++ META.yml.pl
@@ -3,44 +3,78 @@
 use strict;
 use warnings;
 
-use File::Slurp;
+use YAML;
 
-print <<"EOT";
---- #YAML:1.0
-name: incline
-abstract: a replicator for RDB shards
-version: @{[ do {
-    my $s = `src/incline --version`;
-    chomp $s;
-    $s;
-}]}
-author:
-@{[
-    join "\n", map {
-        "  - $_"
-    } split /\n/, read_file('AUTHORS')
-]}
-license: bsd
-distribution_type: script
-dynamic_config: 0
-requires:
-  File::Slurp: 0
-  perl: 5.008
-build_requires:
-  DBI: 0
-  List::MoreUtils: 0
-  Scope::Guard: 0
-  Test::mysqld: 0
-  Test::postgresqld: 0
-resources:
-  license: http://www.opensource.org/licenses/bsd-license.php
-no_index:
-  directory:
-    - example
-    - src
-    - t
-meta-spec:
-  version: 1.4
-  url: http://module-build.sourceforge.net/META-spec-v1.4.html
-generated_by: META.yml.pl
+my $meta = {
+    name               => 'incline',
+    abstract           => 'a replicator for RDB shards',
+    version            => do {
+        my $s = `src/incline --version`;
+        chomp $s;
+        $s;
+    },
+    author             => do {
+        open my $fh, '<', 'AUTHORS'
+            or die "failed to open AUTHORS:$!";
+        my @authors = map {
+            chomp $_;
+            $_;
+        } <$fh>;
+        close $fh;
+        \@authors;
+    },
+    license            => 'bsd',
+    distribution_type  => 'script',
+    dynamic_config     => 0,
+    configure_requires => {
+        perl          => 5.008,
+        YAML          => 0,
+    },
+    requires           => {},
+    build_requires     => {
+        DBI                => 0,
+        'List::MoreUtils'  => 0,
+        'Scope::Guard'     => 0,
+        'Test::mysqld'     => 0,
+        'Test::postgresql' => 0,
+    },
+    resources          => {
+        license => 'http://www.opensource.org/licenses/bsd-license.php',
+    },
+    no_index           => {
+        directory => [ qw/example src t/ ],
+    },
+    'meta-spec'        => {
+        version => 1.4,
+        url => 'http://module-build.sourceforge.net/META-spec-v1.4.html',
+    },
+    generated_by       => 'META.yml.pl',
+};
+
+if (@ARGV && $ARGV[0] eq '--fix-makefile') {
+
+    my $prereq_expr = do {
+        my %req = (
+            %{$meta->{requires}},
+            %{$meta->{build_requires}},
+        );
+        join ", ", map { "$_=>q[$req{$_}]" } sort keys %req;
+    };
+    print <<"EOT";
+# MakeMaker Parameters:
+
+#  PREREQ_PM => { $prereq_expr }
+
+# --- MakeMaker post_initialize section:
+
 EOT
+    while (my $l = <STDIN>) {
+        print $l;
+    }
+
+} else {
+
+    print Dump($meta);
+
+}
+
