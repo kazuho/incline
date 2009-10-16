45ce4417d8817b33dc449d963fb881d93f2d6b05
kazuho <kazuho@4d3e2a30-9d6d-0410-bc8c-dac56cff10b3>
2009-10-16 05:35:07 +0000

    parallelize benchmark
    git-svn-id: http://kazuho.31tools.com/svn/incline/trunk@215 4d3e2a30-9d6d-0410-bc8c-dac56cff10b3

--- benchmark/pivot-standalone.pl
+++ benchmark/pivot-standalone.pl
@@ -10,7 +10,7 @@ use DBI;
 use InclineTest;
 
 my $ROWS = 10_000;
-my $ROWS_PER_STMT = 100;
+my $NUM_WORKERS = 10;
 
 # setup
 my $db = init_db(
@@ -28,14 +28,16 @@ my $db = init_db(
         port => 19010,
     },
 );
-my $dbh = DBI->connect($db->dsn)
-    or die $DBI::errstr;
-$dbh->do('CREATE TABLE d (x INT NOT NULL,y INT NOT NULL,PRIMARY KEY (x,y),UNIQUE (y,x))')
-    or die $dbh->errstr;
-$dbh->do('CREATE TABLE i_s (x INT NOT NULL,y INT NOT NULL,PRIMARY KEY (x,y))')
-    or die $dbh->errstr;
-$dbh->do('CREATE TABLE i_d (y INT NOT NULL,x INT NOT NULL,PRIMARY KEY (y,x))')
-    or die $dbh->errstr;
+{
+    my $dbh = DBI->connect($db->dsn)
+        or die $DBI::errstr;
+    $dbh->do('CREATE TABLE d (x INT NOT NULL,y INT NOT NULL,PRIMARY KEY (x,y),UNIQUE (y,x))')
+        or die $dbh->errstr;
+    $dbh->do('CREATE TABLE i_s (x INT NOT NULL,y INT NOT NULL,PRIMARY KEY (x,y))')
+        or die $dbh->errstr;
+    $dbh->do('CREATE TABLE i_d (y INT NOT NULL,x INT NOT NULL,PRIMARY KEY (y,x))')
+        or die $dbh->errstr;
+}
 system(
     qw(src/incline),
     "--rdbms=$ENV{TEST_DBMS}",
@@ -43,66 +45,113 @@ system(
        create-trigger),
 ) == 0 or die "src/incline failed: $?";
 
-sub do_insert {
-    my $table = shift;
-    my @rows;
-    for (my $i = 0; $i < $ROWS; $i++) {
-        push @rows, "($i," . ($i * 33 % $ROWS) . ')';
-        if (@rows == $ROWS_PER_STMT) {
-            $dbh->do("INSERT INTO $table (x,y) VALUES " . join(',', @rows))
-                or die $dbh->errstr;
-            @rows = ();
+sub do_parallel {
+    my $code = shift;
+    my %pids;
+    for (my $i = 0; $i < $NUM_WORKERS; $i++) {
+        defined(my $pid = fork)
+            or die "fork failed:$!";
+        if ($pid == 0) {
+            # child process
+            $code->($i);
+            exit 0;
+        }
+        $pids{$pid} = 1;
+    }
+    while (%pids) {
+        if ((my $pid = wait) != -1) {
+            delete $pids{$pid};
         }
     }
 }
 
+sub do_insert {
+    my ($table, $rows_per_stmt) = @_;
+    do_parallel(
+        sub {
+            my $base = shift(@_) * $ROWS / $NUM_WORKERS;
+            my $dbh = DBI->connect($db->dsn)
+                or die $DBI::errstr;
+            my @rows;
+            for (my $i = 0; $i < $ROWS / $NUM_WORKERS; $i++) {
+                my $x = $i + $base;
+                push @rows, "($x," . ($x * 33 % $ROWS) . ')';
+                if (@rows == $rows_per_stmt) {
+                    $dbh->do(
+                        "INSERT INTO $table (x,y) VALUES " . join(',', @rows),
+                    ) or die $dbh->errstr;
+                    @rows = ();
+                }
+            }
+        },
+    );
+}
+
 sub do_delete {
-    my $table = shift;
-    for (my $i = 0; $i < $ROWS; $i += $ROWS_PER_STMT) {
-        $dbh->do(
-            "DELETE FROM $table WHERE $i<=x AND x<" . ($i + $ROWS_PER_STMT),
-        ) or die $dbh->errstr;
-    }
+    my ($table, $rows_per_stmt) = @_;
+    do_parallel(
+        sub {
+            my $base = shift(@_) * $ROWS / $NUM_WORKERS;
+            my $dbh = DBI->connect($db->dsn)
+                or die $DBI::errstr;
+            for (my $i = 0; $i < $ROWS / $NUM_WORKERS; $i += $rows_per_stmt) {
+                my $x = $i + $base;
+                $dbh->do(
+                    "DELETE FROM $table WHERE $x<=x AND x<"
+                        . ($x + $rows_per_stmt),
+                ) or die $dbh->errstr;
+            }
+        },
+    );
 }
 
 my %bench;
 # benchmark
 for (my $i = 0; $i < 1; $i++) {
     # direct
-    for (1, 10, 100) {
-        $ROWS_PER_STMT = $_;
+    for my $rows_per_stmt (1, 10, 100) {
+        die "condition check failed"
+            unless $ROWS % ($NUM_WORKERS * $rows_per_stmt) == 0;
         print STDERR ".";
         push_bench(
-            ($bench{"insert ($ROWS_PER_STMT rows)"} ||= {}),
+            ($bench{"insert ($rows_per_stmt rows)"} ||= {}),
             'direct',
-            sub { do_insert('d') },
+            sub { do_insert('d', $rows_per_stmt) },
         );
         print STDERR ".";
         push_bench(
-            ($bench{"delete ($ROWS_PER_STMT rows)"} ||= {}),
+            ($bench{"delete ($rows_per_stmt rows)"} ||= {}),
             'direct',
-            sub { do_delete('d') },
+            sub { do_delete('d', $rows_per_stmt) },
         );
-        $dbh->selectrow_arrayref('SELECT COUNT(*) FROM d')->[0] == 0
+        DBI->connect($db->dsn)->selectrow_arrayref(
+            'SELECT COUNT(*) FROM d',
+        )->[0] == 0
             or die "logic flaw";
         # incline
         print STDERR ".";
         push_bench(
-            ($bench{"insert ($ROWS_PER_STMT rows)"} ||= {}),
+            ($bench{"insert ($rows_per_stmt rows)"} ||= {}),
             'incline',
-            sub { do_insert('i_s') },
+            sub { do_insert('i_s', $rows_per_stmt) },
         );
-        $dbh->selectrow_arrayref('SELECT COUNT(*) FROM i_d')->[0] == $ROWS
+        DBI->connect($db->dsn)->selectrow_arrayref(
+            'SELECT COUNT(*) FROM i_d',
+        )->[0] == $ROWS
             or die "logic flaw";
         print STDERR ".";
         push_bench(
-            ($bench{"delete ($ROWS_PER_STMT rows)"} ||= {}),
+            ($bench{"delete ($rows_per_stmt rows)"} ||= {}),
             'incline',
-            sub { do_delete('i_s') },
+            sub { do_delete('i_s', $rows_per_stmt) },
         );
-        $dbh->selectrow_arrayref('SELECT COUNT(*) FROM i_s')->[0] == 0
+        DBI->connect($db->dsn)->selectrow_arrayref(
+            'SELECT COUNT(*) FROM i_s',
+        )->[0] == 0
             or die "logic flaw";
-        $dbh->selectrow_arrayref('SELECT COUNT(*) FROM i_d')->[0] == 0
+        DBI->connect($db->dsn)->selectrow_arrayref(
+            'SELECT COUNT(*) FROM i_d',
+        )->[0] == 0
             or die "logic flaw";
     }
 }
@@ -116,6 +165,4 @@ for my $n (sort keys %bench) {
     print "\n";
 }
 
-$dbh->disconnect;
-undef $dbh;
 undef $db;
